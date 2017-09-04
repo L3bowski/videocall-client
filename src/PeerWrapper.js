9d0d9f3f45036043b2b02f42e817cfa5ec8c22b3
Carles Capellas Careta <capellas.carles@gmail.com>
2017-09-04 20:03:55 +0200

    Extract stream code into separate functions

--- src/PeerWrapper.js
+++ src/PeerWrapper.js
@@ -6,16 +6,9 @@ export default class PeerWrapper {
 		this.webSocketWrapper = webSocketWrapper;
 		this.peerConnection = new RTCPeerConnection();
 
-		this.peerConnection.onaddstream = (event) => {
-            let videoWrapper = document.getElementById('video-wrapper');
-            videoWrapper.innerHTML = '';
-            let video = document.createElement('video');
-            videoWrapper.appendChild(video);
-            video.src = window.URL.createObjectURL(event.stream);
-            video.play();
-        };
+		this.peerConnection.onaddstream = event => this.consumeVideoStream('video-wrapper', event.stream);
 
-        this.peerConnection.onicecandidate = (event) => {
+        this.peerConnection.onicecandidate = event => {
             if (event.candidate) {
                 this.webSocketWrapper.sendMessage({
                     operationType: 'ice',
@@ -27,17 +20,32 @@ export default class PeerWrapper {
         };
 	}
 
+    consumeVideoStream(elementId, stream) {
+        let videoWrapper = document.getElementById(elementId);
+        videoWrapper.innerHTML = '';
+        let video = document.createElement('video');
+        videoWrapper.appendChild(video);
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+        return video;
+    }
+
+    async getVideoStream() {
+        let stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        });
+        return stream;
+    }
+
 	async call(localUserId, remoteUserId) {
 
 		this.localUserId = localUserId;
 		this.remoteUserId = remoteUserId;
 
         /*An stream from the current browser webcam is created and added to the peerConnection*/
-        let stream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        });
-        this.peerConnection.addStream(stream);
+        var videoStream = await this.getVideoStream();
+        this.peerConnection.addStream(videoStream);
 
         let offer = await this.peerConnection.createOffer();
         /*The following operation will create many ice candidates */
