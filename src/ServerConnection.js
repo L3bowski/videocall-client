20cc72808e2278ad611163e9add55551664c1644
Carles Capellas Careta <capellas.carles@gmail.com>
2017-09-05 07:03:56 +0200

    Extract anonymous functions into class methods

--- src/ServerConnection.js
+++ src/ServerConnection.js
@@ -3,80 +3,11 @@ import PeerWrapper from './PeerWrapper.js';
 
 export default class ServerConnection {
 
-	constructor(webSocketUrl, callbacks) {
-		this.localUserId = null;
-		this.remoteUserId = null;
-		this.callStarted = false;
-		this.callbacks = callbacks;
-		this.webSocketWrapper = new WebSocketWrapper(webSocketUrl, message => {
-            let data = JSON.parse(message.data);
-            switch (data.operationType) {
-				case 'userRegistered':
-		        	this.callbacks.userRegistered(data.user, data.otherUsers);
-		        	break;
-
-		        case 'otherUserRegistered':
-		        	this.callbacks.otherUserRegistered(data.user);
-		        	break;
-
-		        case 'requestCall':
-		        	this.callbacks.callRequested(data);
-		        	break;
-
-		        case 'acceptCall':
-		        	this.callbacks.callAccepted(data);
-		        	break;
-
-		        case 'ice':
-		        	this.peerWrapper.addIceCandidate(data.candidate);
-		        	break;
-
-		        case 'offer':
-		            // IIFE lamda to synchronously exectue asynchronous functions
-            		(async () => {
-            			await this.respondCall(data);
-            		})();
-		            break;
-
-		        case 'answer':
-		        	// IIFE lamda to synchronously exectue an asynchronous functions
-            		(async () => {
-            			await this.peerWrapper.setRemoteDescription(data.answer);
-            		})();
-		        	break;
-            }
-        });
-
-        this.peerWrapper = new PeerWrapper(candidate => {
-        	this.webSocketWrapper.sendMessage({
-	            operationType: 'ice',
-	            candidate,
-	            senderId: this.localUserId,
-	            receiverId: this.remoteUserId
-	        });
-        });
-	}
-
-    register(username) {
-        this.webSocketWrapper.sendMessage({
-            operationType: 'register',
-        	username
-        });
-    }
-
-    requestCall(senderId, receiverId) {
-		this.webSocketWrapper.sendMessage({
-            operationType: 'requestCall',
-        	receiverId,
-        	senderId
-        });
-    }
-
-    acceptCall(initiatorId, userId) {
+    acceptCall(remoteUserId, localUserId) {
 		this.webSocketWrapper.sendMessage({
             operationType: 'acceptCall',
-        	receiverId: initiatorId,
-        	senderId: userId
+        	receiverId: remoteUserId,
+        	senderId: localUserId
         });
     }
 
@@ -92,6 +23,78 @@ export default class ServerConnection {
             offer,
             senderId: localUserId,
             receiverId: remoteUserId
+        });
+    }
+
+	constructor(webSocketUrl, callbacks) {
+		this.localUserId = null;
+		this.remoteUserId = null;
+		this.callStarted = false;
+		this.callbacks = callbacks;
+		this.webSocketWrapper = new WebSocketWrapper(webSocketUrl, this.onWebsocketMessage.bind(this));
+        this.peerWrapper = new PeerWrapper(this.iceCandidateHanlder.bind(this));
+	}
+
+	iceCandidateHanlder(iceCandidate) {
+    	this.webSocketWrapper.sendMessage({
+            operationType: 'ice',
+            iceCandidate,
+            senderId: this.localUserId,
+            receiverId: this.remoteUserId
+        });
+	}
+
+	onWebsocketMessage(message) {
+		let data = JSON.parse(message.data);
+        switch (data.operationType) {
+			case 'userRegistered':
+	        	this.callbacks.userRegistered(data.user, data.otherUsers);
+	        	break;
+
+	        case 'otherUserRegistered':
+	        	this.callbacks.otherUserRegistered(data.user);
+	        	break;
+
+	        case 'requestCall':
+	        	this.callbacks.callRequested(data);
+	        	break;
+
+	        case 'acceptCall':
+	        	this.callbacks.callAccepted(data);
+	        	break;
+
+	        case 'ice':
+	        	this.peerWrapper.addIceCandidate(data.iceCandidate);
+	        	break;
+
+	        case 'offer':
+	            // IIFE lamda to synchronously exectue asynchronous functions
+        		(async () => {
+        			await this.respondCall(data);
+        		})();
+	            break;
+
+	        case 'answer':
+	        	// IIFE lamda to synchronously exectue an asynchronous functions
+        		(async () => {
+        			await this.peerWrapper.setRemoteDescription(data.answer);
+        		})();
+	        	break;
+        }
+	}
+
+    register(username) {
+        this.webSocketWrapper.sendMessage({
+            operationType: 'register',
+        	username
+        });
+    }
+
+    requestCall(localUserId, remoteUserId) {
+		this.webSocketWrapper.sendMessage({
+            operationType: 'requestCall',
+        	receiverId: remoteUserId,
+        	senderId: localUserId
         });
     }
 
